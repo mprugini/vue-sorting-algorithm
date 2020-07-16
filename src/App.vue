3f46bcb98cfa349f5868456830516585c145e17f
Milos Pruginic <milos_pruginic@yahoo.com>
2020-07-16 15:20:00 -0400

    Bubble sort using Vuejs

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,78 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <button class="btn btn-primary" @click="shuffle(numberArray)">Shuffle!</button>
+    <button class="btn btn-primary mx-2" @click="bubbleSort(numberArray)">Bubble Sort</button>
+    <button class="btn btn-primary mx-2" @click="selectionSort(numberArray)">Selection Sort</button>
+    <button class="btn btn-primary mx-2" @click="insertionSort">Insertion Sort</button>
+    <br>
+    <br>
+    <ul class="list-group list-group-horizontal justify-content-center">
+      <!-- :class="[ number % 2 === 0 ? 'list-group-item list-group-item-primary' : 'list-group-item list-group-item-success' ] -->
+      <li :class="[ number % 2 === 0 ? 'list-group-item list-group-item-primary' : 'list-group-item list-group-item-success' ]" v-for="number in numberArray" :key="number">{{ number }}</li>
+    </ul>
+    <!-- <br>
+    <br>
+    <ul class="list-group list-group-horizontal justify-content-center">
+      <li :class="[ number % 2 === 0 ? 'list-group-item list-group-item-primary' : 'list-group-item list-group-item-success' ]" v-for="number in shuffledArray" :key="number">{{ number }}</li>
+    </ul> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header
+  },
+  data() {
+    return {
+      numberArray: [1, 2, 3, 4, 5, 6, 7, 8],
+      shuffledArray: []
+    }
+  },
+  methods: {
+    arraySetWithoutIndexes(array, index, value) {
+      array.splice(index, 1, value);
+    },
+
+    arraySwap(array, indexA, indexB) {
+      var x = array[indexA];
+      // array[indexA] = array[indexB];
+      // array[indexB] = x;
+      this.arraySetWithoutIndexes(array, indexA, array[indexB]);
+      this.arraySetWithoutIndexes(array, indexB, x);
+    },
+
+    async shuffle(a) {
+      var j, i;
+      for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        this.arraySwap(a, i, j);
+      }
+    },
+
+    async bubbleSort(a) {
+
+      var len = a.length;
+      for (var i = len - 1; i >= 0; i--) {
+        for (var j = 1; j <= i; j++) {
+          if (a[j - 1] > a[j]) {
+            this.arraySwap(a, j - 1, j);
+          }
+        }
+      }
+
+    },
+
+    async selectionSort(a) {
+      alert(a);
+    },
+    insertionSort() {
+      alert('Insertion sort');
+    }
   }
 }
 </script>
---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+            <h1 class="display-4">Vue Sorting Algorithms</h1>
+            <p class="lead">This is a simple web app for basic sorting algorithms.</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
